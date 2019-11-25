f86f562c72676803b074bcccbf8b190e012d65a9
Shu Hwai Teoh <teohshuhwai@gmail.com>
2019-11-26 05:53:45 +0800

    11/25_17:00

--- source/Bytecode.java
+++ source/Bytecode.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
-
-import com.sun.jdi.Value;
-
 import java.util.*;
-import javafx.util.Pair;
+
 
 public class Bytecode {
 	public static final int HALT = 0;
@@ -29,6 +26,7 @@ public class Bytecode {
 	public static final int SHORT = 0;
 	public static final int INT = 1;
 	public static final int FLOAT = 2;
+	public static final int RET = 48;
 	//bytecode to  write
 	//int code;
 	// current line being compiled
@@ -40,52 +38,204 @@ public class Bytecode {
 	// lines
 	ArrayList<String> source = new ArrayList<String>();
 	// opcodes
-	ArrayList<Integer> mem = new ArrayList<Integer>();
-	Pair <Integer, Integer> value = new Pair<Integer, Integer>(0,0);
-	Map<String, Value> symbol_table;
-	ArrayList<ArrayList<Integer>> value = new ArrayList<ArrayList<Integer>>();
-	Map<String, value> unknown_labels;
-	int decl(String, int);
-	int lab(String);
-	int subr(int, String);
-	int ret();
-	int printi(int);
-	int printv(String);
-	int jmp(String);
-	int jmpc(String);
-	int cmpe();
-	int cmplt();
-	int cmpgt();
-	int pushi(int);
-	int pushvi(String);
-	int popm(int);
-	int popa(int);
-	int popv(String);
-	int peek(String, int);
-	int poke(int, String);
-	int swp();
-	int add();
-	int sub();
-	int mul();
-	int div();
-	int parse(String);
-	String[] find_tokens(String);
-	boolean is_alpha(String);
-	// destination from slide 3 is passed in
-	int compile(ArrayList<byte>);
-}
+	public ArrayList<Integer> mem = new ArrayList<Integer>();
+	int[] value = {0,0};
+	Map<String, int[]> symbol_table;
+	//ArrayList<ArrayList<Integer>> value = new ArrayList<ArrayList<Integer>>();
+	Map<String, Integer> unknown_labels;
+	
+	public Bytecode(ArrayList<String> s) {
+		source = s;
+	}
+	
+	int decl(String var, int type){
+		String var_name = "main" + "_" + var;
+		int[] v = {++fo, type};
+		symbol_table.put(var_name, v);
+		// reserve a space in the stack for the variable by generating the following code
+		pushi(0);
+		return 0;
+	}
+	
+	int lab(String lable) {
+		int[] v = {++fo, LABEL};
+		if (unknown_labels.get(lable) == null) {
+			String key = "main" + "_" + lable;
+			symbol_table.put(key, v);
+		}
+		else {
+			symbol_table.put(lable, v);
+		}
+		return 0;
+	}
+	
+	int subr(int argNum, String flabel) {
+		String k = "main" + "_" + flabel;
+		int[] v = {++fo, argNum};
+		symbol_table.put(k, v);
+		return 0;
+	}
+	
+	int ret() {
+		pushi(0);
+		mem.add(POPA);
+		mem.add(HALT);
+		pc = pc+2;
+		return 0;
+	}
+	
+	int printi(int literal) {
+		pushi(0);
+		mem.add(PRINTI);
+		pc += 1;
+		return 0;
+	}
+	int jmp(String label) {
+		int[] p;
+		p = symbol_table.get(label);
+		if (p != null) {
+			pushi(p[0]);
+			mem.add(JMP);
+			pc++;
+		}
+		else {
+			String label_name = "main" + "_" + label;
+			unknown_labels.put(label_name, 0);
+		}
+		return 0;
+	}
+	int jmpc(String label) {
+		int[] p;
+		p = symbol_table.get(label);
+		if (p != null) {
+			pushi(p[0]);
+			mem.add(JMP);
+			pc++;
+		}
+		else {
+			String label_name = "main" + "_" + label;
+			unknown_labels.put(label_name, 0);
+		}
+		return 0;
+	}
+	int cmpe() {
+		mem.add(CMPE);
+		pc++;
+		return 0;
+	}
+	int cmplt() {
+		mem.add(CMPLT);
+		pc++;
+		return 0;
+	}
+	int cmpgt() {
+		mem.add(CMPGT);
+		pc++;
+		return 0;
+	}
+	
+	int pushi(int v){
+		byte[] result = new byte[4];
 
-int parse(String statement) {
-	// Find operation and operands in current statement
-	String[] tokens = find_tokens(statement); //(return statement.split( );)
-	for(String str: tokens){
-		System.out.println(str + "," );
+		result[0] = (byte) (v >> 24);
+		result[1] = (byte) (v >> 16);
+		result[2] = (byte) (v >> 8);
+		result[3] = (byte) (v /*>> 0*/);
+		mem.add(PUSHI);
+		mem.add((int)result[0]);
+		mem.add((int)result[1]);
+		mem.add((int)result[2]);
+		mem.add((int)result[3]);
+		pc += 5;
+		return 0;
+	}
+	
+	int printv(String var) {
+		int[] p;
+		p = symbol_table.get(var);
+		pushi(p[0]);
+		pushvi(var);
+		//mem.add(PRINTV);
+		return 0;
+	}
+	
+	int pushvi(String var) {
+		int[] p;
+		p = symbol_table.get(var);
+		pushi(p[0]);
+		mem.add(PUSHVI);
+		pc++;
+		return 0;
+	}
+	
+	int popm(int val) {
+		pushi(val);
+		mem.add(POPM);
+		pc++;
+		return 0;
+	}
+	int popv(String label) {
+		int[] p;
+		p = symbol_table.get(label);
+		pushi(p[0]);
+		mem.add(POPV);
+		pc++;
+		return 0;
+	}
+	int peek(String var, int val) {
+		int[] p;
+		p = symbol_table.get(var);
+		pushi(p[0]);
+		pushi(val);
+		mem.add(PEEKI);
+		pc++;
+		return 0;
+	}
+	int poke(String var, int val) {
+		int[] p;
+		p = symbol_table.get(var);
+		pushi(p[0]);
+		pushi(val);
+		mem.add(POKEI);
+		pc++;
+		return 0;	
+	}
+	int swp() {
+		mem.add(SWP);
+		pc++;
+		return 0;
+	}
+	int add() {
+		mem.add(ADD);
+		pc++;
+		return 0;
+	}
+	int sub() {
+		mem.add(SUB);
+		pc++;
+		return 0;
+	}
+	int mul() {
+		mem.add(MUL);
+		pc++;
+		return 0;
+	}
+	int div() {
+		mem.add(DIV);
+		pc++;
+		return 0;
+	}
+	
+	void parse(String statement) {
+		// Find operation and operands in current statement
+		String[] tokens = find_tokens(statement); //(return statement.split( );)
+//		for(String str: tokens){
+//			System.out.println(str + "," );
+//		}
 		String operation = tokens[0];
 		switch(operation) {
 			case("decl"):
 				if (tokens[2] == "int") decl(tokens[1], INT);
-				//if (tokens[2] == "short") {decl(tokens[1], SHORT);}
-				//if (tokens[2] == "float") {decl(tokens[1], FLOAT);}
 				break;
 			case("lab"):
 				lab(tokens[1]);
@@ -97,7 +247,7 @@ int parse(String statement) {
 				ret();
 				break;			
 			case("printi"):
-				printi(Integer.parseInt(tokens[1]));
+				printi(0);
 				break;
 			case("printv"):
 				printv(tokens[1]);
@@ -118,8 +268,8 @@ int parse(String statement) {
 				cmpgt();
 				break;	
 			case("pushi"):
-				int value = Integer.parseInt(tokens[1]);
-				pushi(value);
+				int v = Integer.parseInt(tokens[1]);
+				pushi(v);
 				break;		
 			case("pushvi"):
 				pushvi(tokens[1]);
@@ -128,13 +278,13 @@ int parse(String statement) {
 				popm(Integer.parseInt(tokens[1]));
 				break;
 			case("popv"):
-				pushvi(tokens[1]);
+				popv(tokens[1]);
 				break;
 			case("peek"):
 				peek(tokens[1], Integer.parseInt(tokens[2]));
 				break;
-			case("popv"):
-				pushvi(tokens[1], Integer.parseInt(tokens[2]));
+			case("poke"):
+				poke(tokens[1], Integer.parseInt(tokens[2]));
 				break;
 			case("swp"): 
 				swp();
@@ -152,176 +302,27 @@ int parse(String statement) {
 				div();
 				break;
 
+		}
 	}
-	//cmpe, cmplt, cmpgt, swp, add, sub, mul, div (tokens.size() == 1)
-	//lab, printi, jmp, jmpc, pushi, pushvi, popm, popv (tokens.size() == 2)
-	//decl, subr, peek, poke (tokens.size() == 3)
+	
+	String[] find_tokens(String statement){
+		String[] tokens = statement.split(" ");
+		return tokens;
 	}
-}
-
-String[] find_tokens(String statement){
-	String[] tokens = statement.split(" ");
-	return tokens;
-}
-
-boolean is_alpha(String);
-
-int compile(ArrayList<byte> des) {
-	int i = 0;
-	for (String str : source){
-		sc = i + 1;
-		parse(str);
-		// Check that source code ends with RET
-		// save mem into des (destination in slide 3 -> compile result saved in .smp file)
-		des = mem;
+	
+	int compile() {
+		int i = 0;
+		for (String str : source){
+			sc = i + 1;
+			parse(str);
+			// Check that source code ends with RET
+			// save mem into des (destination in slide 3 -> compile result saved in .smp file)
+			//des = (byte)mem;
+		}
+		return 0;
 	}
-}
+	
 
-int decl(String var, int type){
-	String var_name = "main" + "_" + var;
-	int[] value = {++fo, type};
-	symbol_table.put(var_name, value);
-	// reserve a space in the stack for the variable by generating the following code
-	pushi(0);
-	//if(type == "SHORT") pushs(0);
-	//if(type == "FLOAT") pushf(0);
-	return 0;
-}
-
-int lab(String lable) {
-	String key = "main" + "_" + lable;
-	int[] value = {++fo, LABEL};
-	symbol_table.put(key, value);
-	return 0;
-}
-
-int subr(int argNum, String flabel) {
-	String key = "main" + "_" + flabel;
-	int[] value = {++fo, argNum};
-	symbol_table.put(key, value);
-	return 0;
-}
-
-int ret() {
-	pushi(0);
-	popa(0);
-	return 0;
-}
-
-int printi(int literal) {
-	//Integer [] bytes = Arrays.copyof(literal, 4);
-	mem.add(PRINTI);
-//	mem.add(bytes[0]);
-//	mem.add(bytes[1]);
-//	mem.add(bytes[2]);
-//	mem.add(bytes[3]);
-	pc += 1;
-}
-int jmp(String label) {
-	int p;
-	p = symbol_table.get(label);
-	pushi(p.getKey());
-	mem.add(JMP);
-	pc++;
-}
-int jmpc(String label) {
-	int p;
-	p = symbol_table.get(label);
-	pushi(p.getKey());
-	mem.add(JMPC);
-	pc++;
-}
-int cmpe() {
-	mem.add(CMPE);
-	pc++;
-}
-int cmplt() {
-	mem.add(CMPLT);
-	pc++;
-}
-int cmpgt() {
-	mem.add(CMPGT);
-	pc++;
-}
-
-int pushi(int value){
-	Integer[] bytes = Arrays.copyof(value, value.length);
-	mem.add(PUSHI);
-	mem.add(bytes[0]);
-	mem.add(bytes[1]);
-	mem.add(bytes[2]);
-	mem.add(bytes[3]);
-	pc += 5;
-	return 0;
-}
-
-int printv(String var) {
-	int p;
-	p = symbol_table.get(var);
-	pushi(p.getKey());
-	pushv(p.getValue());
-	//mem.add(PRINTV);
-	return 0;
-}
-
-int pushvi(String var) {
-	int p;
-	p = symbol_table.get(var);
-	pushi(p.getKey());
-	mem.add(PUSHVI);
-	pc++;
-}
-
-int popm(int val) {
-	pushi(val);
-	mem.add(POPM);
-	pc++;
-}
-int popa(int);
-int popv(String label) {
-	int p;
-	p = symbol_table.get(label);
-	pushi(p.getKey());
-	mem.add(POPV);
-	pc++;
-}
-int peek(String var, int val) {
-	int p;
-	p = symbol_table.get(var);
-	pushi(p.getKey());
-	pushi(val);
-	mem.add(PEEKI);
-	pc++;
-	return 0;
-}
-int poke(String var, int val) {
-	int p;
-	p = symbol_table.get(var);
-	pushi(p.getKey());
-	pushi(val);
-	mem.add(POKEI);
-	pc++;
-	return 0;	
-}
-int swp() {
-	mem.add(SWP);
-	pc++;
-}
-int add() {
-	mem.add(ADD);
-	pc++;
-}
-int sub() {
-	mem.add(SUB);
-	pc++;
-}
-int mul() {
-	mem.add(MUL);
-	pc++;
-}
-int div() {
-	mem.add(DIV);
-	pc++;
 }
 
 
--- source/Main.java
+++ source/Main.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class Main{
    public static void main(String[ ] args) {
 	   File filename = new File(args[0]);//add path
 	   ArrayList<String> source = new ArrayList<String>();
-	   String statement;
+	   //String statement;
 	   BufferedReader reader;
+	   byte[] data = {0};
+	   int idx = 0;
 	   
 	   try{
 		   reader = new BufferedReader(new FileReader(filename));
@@ -20,9 +23,23 @@ public class Main{
 		   }
 	   }
 	   catch(IOException e){e.printStackTrace();}
-	   Bytecode bc = Bytecode(source);
+	   Bytecode bc = new Bytecode(source);
 	   
-	   ArrayList<Byte> destination = new ArrayList<Byte>();
-	   bc.compile(destination);
+	   //ArrayList<Byte> destination = new ArrayList<Byte>();
+	  // bc.compile(destination);
+	   bc.compile();
+	   for (int i=0;i<bc.mem.size();i++) {
+		   data[idx++] = (byte) bc.mem.indexOf(i);
+	   }
+
+	   try {
+           OutputStream out = new FileOutputStream("output.smp"); 
+           out.write(data); 
+           System.out.println("Successfully byte inserted"); 
+           out.close(); 
+       } 
+       catch (Exception e) { 
+           System.out.println("Exception: " + e); 
+       } 
    }
 }
